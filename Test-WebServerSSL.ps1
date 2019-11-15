cb3d3babd04e9119464073ecdf363ed441b7315c
Vasyl <vegas.firestorm@gmail.com>
2019-11-15 10:36:43 +0200

    Update Test-WebServerSSL.ps1

--- Test-WebServerSSL.ps1
+++ Test-WebServerSSL.ps1
@@ -4,14 +4,14 @@ function Test-WebServerSSL {
         [string]$URL
 	)
 	
-	[Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
-	$req = [Net.HttpWebRequest]::Create($URL)
-	$req.AllowAutoRedirect = $false
-	try {$req.GetResponse() |Out-Null} catch {Write-Host Exception while checking URL $url`: $_ -f Red}
-	$certName = $req.ServicePoint.Certificate.Subject.Split(', ',[System.StringSplitOptions]::RemoveEmptyEntries)[0].Split('=')[1]
-	$certThumbprint = $req.ServicePoint.Certificate.GetCertHashString()
-	Write-Host "CN is $certName"
-	Write-Host "Thumbprint is $certThumbprint"
+    [Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+    $req = [Net.HttpWebRequest]::Create($URL)
+    $req.AllowAutoRedirect = $false
+    try {$req.GetResponse() |Out-Null} catch {Write-Host Exception while checking URL $url`: $_ -f Red}
+    $certName = $req.ServicePoint.Certificate.Subject.Split(', ',[System.StringSplitOptions]::RemoveEmptyEntries)[0].Split('=')[1]
+    $certThumbprint = $req.ServicePoint.Certificate.GetCertHashString()
+    Write-Host "CN is $certName"
+    Write-Host "Thumbprint is $certThumbprint"
 }
 
 $URL = "https://wrong.host.badssl.com/"
